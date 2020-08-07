6016ab9737c26ffc2454080f19216d38e4211528
phyo-kyaw <phy0ky6w@gmail.com>
2020-08-07 23:34:42 +1000

    slide-69

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit, OnDestroy {
+  // posts = [
+  //   {title:'1st Post', content: 'This is the 1st post\'s content.'},
+  //   {title:'2nd Post', content: 'This is the 2nd post\'s content.'},
+  //   {title:'3rd Post', content: 'This is the 3rd post\'s content.'}
+  // ];
+  posts: Post[] =[];
+  private postsSub: Subscription;
+
+  constructor(public postsService: PostsService) {}
+
+  ngOnInit() {
+    this.postsService.getPosts();
+    this.postsSub = this.postsService.getPostUpdateListener()
+      .subscribe( (posts: Post[]) => {
+        this.posts=posts;
+      });
+  }
+
+  onDelete(postId: string ) {
+    this.postsService.deletePost(postId);
+  }
+
+  ngOnDestroy() {
+    this.postsSub.unsubscribe();
+  }
+}
